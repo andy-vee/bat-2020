260374ef770ebcbb726b243bef0fae15757c3400
Andy Vance <anvance@gmail.com>
2021-06-09 20:57:09 -0400

    adding parser functions and other stuff

---
+++ code/listing_parsers.py
@@ -0,0 +1,25 @@
+def active_listing_parser(url):
+    import pandas as pd, lxml
+    tables = pd.read_html(url)
+    auction_stats_table = tables[0]
+    new_dict = dict(zip(auction_stats_table[0], auction_stats_table[1]))
+    new_dict['Time Left'] = new_dict['Time Left'].split('  ')[0]
+    new_dict['Ends On'] = new_dict['Ends On'].split('  ')[0]
+    del(new_dict['Place Bid'], new_dict['Time Left'])
+    view_stats = tables[1].loc[0,1]
+    new_dict['Views'], new_dict['Watchers'] = view_stats.replace(',','').replace(' views','').replace(' watchers','').split(' | ')[:]
+    return(new_dict)
+
+def closed_listing_parser(url):
+    import pandas as pd, lxml
+    tables = pd.read_html(url)
+    auction_stats_table = tables[0]
+    new_dict = dict(zip(auction_stats_table[0], auction_stats_table[1]))
+    try:
+        new_dict['Winner'] = new_dict['Winning Bid'].split(' by ')[1]
+        new_dict['Winning Bid'] = new_dict['Winning Bid'].split(' by ')[0]
+    except KeyError:
+        pass
+    view_stats = tables[1].loc[0,0]
+    new_dict['Views'], new_dict['Watchers'] = view_stats.replace(',','').replace(' views','').replace(' watchers','').split(' | ')[:]
+    return(new_dict)

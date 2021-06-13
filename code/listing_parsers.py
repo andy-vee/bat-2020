cc379b964dd44996733347c61c93cd7e0dd35156
Andy Vance <anvance@gmail.com>
2021-06-13 11:21:19 -0400

    added timestamp element to parsers

--- code/listing_parsers.py
+++ code/listing_parsers.py
@@ -1,10 +1,12 @@
 def active_listing_parser(url):
     import pandas as pd, lxml
+    from datetime import datetime
     tables = pd.read_html(url)
     auction_stats_table = tables[0]
     new_dict = dict(zip(auction_stats_table[0], auction_stats_table[1]))
     new_dict['Time Left'] = new_dict['Time Left'].split('  ')[0]
     new_dict['Ends On'] = new_dict['Ends On'].split('  ')[0]
+    new_dict['listing_scrape_ts'] = datetime.now()
     del(new_dict['Place Bid'], new_dict['Time Left'])
     view_stats = tables[1].loc[0,1]
     new_dict['Views'], new_dict['Watchers'] = view_stats.replace(',','').replace(' views','').replace(' watchers','').split(' | ')[:]
@@ -12,12 +14,14 @@ def active_listing_parser(url):
 
 def closed_listing_parser(url):
     import pandas as pd, lxml
+    from datetime import datetime
     tables = pd.read_html(url)
     auction_stats_table = tables[0]
     new_dict = dict(zip(auction_stats_table[0], auction_stats_table[1]))
     try:
         new_dict['Winner'] = new_dict['Winning Bid'].split(' by ')[1]
         new_dict['Winning Bid'] = new_dict['Winning Bid'].split(' by ')[0]
+        new_dict['listing_scrape_ts'] = datetime.now()
     except KeyError:
         pass
     view_stats = tables[1].loc[0,0]
